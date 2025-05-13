a47fe35ae4fcf34a500b2a0553f5ac915829f55f
Ulysse Magneron <ulmagner@f4r3s5.clusters.42paris.fr>
2025-05-13 14:00:34 +0200

    [MOD] direction implementation

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: ulmagner <ulmagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:54:39 by ulmagner          #+#    #+#             */
-/*   Updated: 2025/05/13 12:16:20 by ulmagner         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:58:56 by ulmagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,46 +27,44 @@ int	movement_r(int keycode, t_all *all)
 	return (0);
 }
 
-static void	direction(t_player *player, t_map *next, int dirx, int diry)
+static void	direction(t_player *player, double dirx, double diry)
 {
-	if (next->i != '1')
-	{
-		if (dirx)
-		{
-			player->x += dirx * player->ms;
-			if ((dirx > 0 && player->x >= next->x)
-				|| (dirx < 0 && player->x <= next->x))
-			{
-				printf("player [x:y] [%f %f] case\n", player->x, player->y);
-				player->h = next;
-			}
-		}
-		else
-		{
-			player->y += diry * player->ms;
-			if ((diry > 0 && player->y >= next->y)
-				|| (diry < 0 && player->y <= next->y))
-			{
-				printf("player [x:y] [%f %f] case\n", player->x, player->y);
-				player->h = next;
-			}
-		}
-	}
+	double (new_x) = player->x + dirx * player->ms;
+	double (new_y) = player->y + diry * player->ms;
+	double (old_x) = player->x;
+	double (old_y) = player->y;
+	if (diry < 0 && new_y < player->h->y && player->h->up->i == '1')
+			new_y = old_y;
+	if (diry > 0 && new_y > player->h->y + 0.9 && player->h->down->i == '1')
+			new_y = old_y;
+	if (dirx < 0 && new_x < player->h->x && player->h->left->i == '1')
+			new_x = old_x;
+	if (dirx > 0 && new_x > player->h->x + 0.9 && player->h->right->i == '1')
+			new_x = old_x;
+	player->x = new_x;
+	player->y = new_y;
+	if (player->y < player->h->y && player->h->i != '1')
+		player->h = player->h->up;
+	else if (player->y > player->h->y + 0.9 && player->h->i != '1')
+		player->h = player->h->down;
+	if (player->x < player->h->x && player->h->i != '1')
+		player->h = player->h->left;
+	else if (player->x > player->h->x + 0.9 && player->h->i != '1')
+		player->h = player->h->right;
 }
 
 int	movement_handling(t_all *all)
 {
+	t_player *(p) = &all->player;
 	if (all->movement.move[XK_w])
-		direction(&all->player, all->player.h->up, 0, -1);
+		direction(p, p->dx, p->dy);
 	if (all->movement.move[XK_s])
-		direction(&all->player, all->player.h->down, 0, 1);
+		direction(p, -p->dx, -p->dy);
 	if (all->movement.move[XK_a])
-		direction(&all->player, all->player.h->left, -1, 0);
+		direction(&all->player, p->dx, -p->dy);
 	if (all->movement.move[XK_d])
-		direction(&all->player, all->player.h->right, 1, 0);
+		direction(&all->player, -p->dx, p->dy);
 	if (all->movement.move[XK_e])
-	{
 		exit((ft_clearall(all), EXIT_FAILURE));
-	}
 	return (1);
 }
